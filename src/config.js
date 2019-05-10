2ee70728c3647a3b1779db9abd02d1d50045aba2
flor <esthelabarbosa@gmail.com>
2019-05-09 23:32:26 -0500

    cambio a root del back

---
+++ src/config.js
@@ -0,0 +1,5 @@
+module.exports.connectionURL = 'mongodb+srv://admin:-Monfebc1997@cluster0-pvezi.mongodb.net/signs?retryWrites=true'
+module.exports.secret = 'secreta123Daniel&Flor:D'
+
+// module.exports.connectionURL = connectionURL
+// module.exports.secret = secret

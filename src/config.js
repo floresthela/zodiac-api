331d32edf4e7b248de646ec66453de0ea0cee5ef
flor <esthelabarbosa@gmail.com>
2019-05-09 23:33:36 -0500

    nope

--- src/config.js
+++
@@ -1,5 +0,0 @@
-module.exports.connectionURL = 'mongodb+srv://admin:-Monfebc1997@cluster0-pvezi.mongodb.net/signs?retryWrites=true'
-module.exports.secret = 'secreta123Daniel&Flor:D'
-
-// module.exports.connectionURL = connectionURL
-// module.exports.secret = secret
